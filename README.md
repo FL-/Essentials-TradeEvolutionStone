52b277cd1f0b4645020132e603ea205de5609e54
FL <fl50000@hotmail.com>
2021-09-18 11:44:17 -0300

    Essentials v19 version

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Trade Evolution Stone
 This script is for Pokémon Essentials. An item who trigger trade evolutions. Trade evolutions with hold item (like Onix to Steelix) only works while holding the correct item (Metal Coat).
 
-![](Files/item901.png)
+![](Files/LINKSTONE.png)
 
 ## Screens
 ![](Screens/gif.gif)
 
 ## Compatibility
-Doesn't work on Essentials version v17 or above.
+Doesn't work on Essentials versions older than v19. To see other Essentials versions, look at [all branches](../../branches/all).
 
 ## Installation
 Follow the [Script](/Script.rb) instructions.
--- Script.rb
+++ Script.rb
@@ -2,39 +2,36 @@
 # * Trade Evolution Stone - by FL (Credits will be apreciated)
 #===============================================================================
 #
-# This script is for Pokémon Essentials. It makes an item who trigger trade
-# evolutions. Trade evolutions with hold item (like Onix to Steelix) only works
-# while holding the correct item (Metal Coat).
+# This script is for Pokémon Essentials. An item who trigger trade evolutions.
+# Trade evolutions with hold item (like Onix to Steelix) only works while
+# holding the correct item (Metal Coat).
 #
-#===============================================================================
+#== INSTALLATION ===============================================================
 #
 # To this script works, put it above main OR convert into a plugin. Add into
 # PBS\items.txt:
 #
-# 901,LINKSTONE,Link Stone,1,2100,"A peculiar stone that makes certain species of Pokémon evolve. It has a link cable pattern.",1,0,0,
+# 901,LINKSTONE,Link Stone,Link Stones,1,3000,"A peculiar stone that makes certain species of Pokémon evolve. It has a link cable pattern.",1,0,7,
 #
 # You can change the fields except the last number. If you change the internal
 # name, change also in the script.
 #
 #===============================================================================
 
-ItemHandlers::UseOnPokemon.add(:LINKSTONE,proc{|item,pokemon,scene|
-   if (pokemon.isShadow? rescue false)
-     scene.pbDisplay(_INTL("It won't have any effect."))
-     next false
-   end
-   newspecies=pbTradeCheckEvolution(pokemon,item)
-   if newspecies<=0
-     scene.pbDisplay(_INTL("It won't have any effect."))
-     next false
-   else
-     pbFadeOutInWithMusic(99999){
-        evo=PokemonEvolutionScene.new
-        evo.pbStartScreen(pokemon,newspecies)
-        evo.pbEvolution(false)
-        evo.pbEndScreen
-        scene.pbRefresh
-     }
-     next true
-   end
-})
+if defined?(PluginManager)
+  PluginManager.register({                                                 
+    :name    => "Trade Evolution Stone",                                        
+    :version => "1.1",                                                     
+    :link    => "https://www.pokecommunity.com/showthread.php?t=458757",             
+    :credits => "FL"
+  })
+end
+
+class Pokemon
+  alias :_old_fl_check_evolution_on_use_item :check_evolution_on_use_item
+  def check_evolution_on_use_item(item_used)
+    # self is a placeholder here
+    return check_evolution_on_trade(self) if item_used==:LINKSTONE
+    return _old_fl_check_evolution_on_use_item(item_used)
+  end
+end
